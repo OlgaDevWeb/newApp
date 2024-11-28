008952b561a54daa18e5d104842d51fc0f46214a
Olga Vertiankina <anficaw@mail.ru>
2024-11-28 16:32:16 +0000

    a

--- src/app/registation/page.tsx
+++ src/app/registation/page.tsx
@@ -5,12 +5,19 @@ import styles from "./page.module.css";
 export default async function Signin() {
   return (
     <div>
-      <Modal>
-        <div className={styles.main}>
-          <h1>Регистрация</h1>
-          <RegForm></RegForm>
-        </div>
-      </Modal>
+      <div className={styles.main}>
+        <h1>Регистрация</h1>
+        <RegForm></RegForm>
+      </div>
     </div>
   );
 }
+
+/*<div>
+<Modal>
+  <div className={styles.main}>
+    <h1>Регистрация</h1>
+    <RegForm></RegForm>
+  </div>
+</Modal>
+</div>*/
--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -6,15 +6,27 @@ import styles from "./page.module.css";
 export default async function Signin() {
   return (
     <div>
-      <Modal>
-        <div className={styles.main}>
-          <h1>Вход</h1>
-          <GoogleButton></GoogleButton>
-          <p className="textbold">или</p>
-          <p className="textbold">Введите почту и пароль</p>
-          <SignInForm></SignInForm>
-        </div>
-      </Modal>
+      <div className={styles.main}>
+        <h1>Вход</h1>
+        <GoogleButton></GoogleButton>
+        <p className="textbold">или</p>
+        <p className="textbold">Введите почту и пароль</p>
+        <SignInForm></SignInForm>
+      </div>
     </div>
   );
 }
+
+/*return (
+  <div>
+    <Modal>
+      <div className={styles.main}>
+        <h1>Вход</h1>
+        <GoogleButton></GoogleButton>
+        <p className="textbold">или</p>
+        <p className="textbold">Введите почту и пароль</p>
+        <SignInForm></SignInForm>
+      </div>
+    </Modal>
+  </div>
+);*/
