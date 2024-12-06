e69ab6aadd146d484b39de8855f9a60b5577f758
Olga Vertiankina <anficaw@mail.ru>
2024-12-06 23:20:58 +0000

    'r'

--- src/configs/auth.ts
+++ src/configs/auth.ts
@@ -2,12 +2,6 @@ import { NextAuthOptions, User } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { prisma } from "@/lib/prisma";
 
-function getAllUser(email: string) {
-  return prisma.user.findUnique({
-    where: { email },
-  });
-}
-
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
 
